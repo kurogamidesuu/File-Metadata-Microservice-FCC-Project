7043ae6ec40ee6ae4457300d96b7ca81328e5c53
kurogamidesuu <hempushpchauhan@gmail.com>
2025-05-16 23:15:04 +0530

    completed project by Hempushp Chauhan

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
 
 var app = express();
 
@@ -11,8 +14,30 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const uploadDir = path.join(__dirname, 'uploads');
 
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, {recursive: true});
+}
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({storage});
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
